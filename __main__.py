295ca5ea4cd0626c045b7297117efa688a5a2b66
drwuro <email@drwuro.com>
2024-01-06 18:10:52 +0100

    added initial class and font etc

--- __main__.py
+++ __main__.py
@@ -1,29 +1,50 @@
 import pygame
-
+from bitmapfont import BitmapFont
 
 SCR_W, SCR_H = 256, 256
 
 pygame.display.init()
 
-window = pygame.display.set_mode((SCR_W, SCR_H), flags=pygame.SCALED)
-
-
 class Game():
     def __init__(self):
-        pass
+        self.window = pygame.display.set_mode((SCR_W, SCR_H), flags=pygame.SCALED)
+        self.running = False
+
+        self.font = BitmapFont('gfx/heimatfont.png', scr_w=SCR_W, scr_h=SCR_H)
+
 
     def render(self):
-        pass
+        self.window.fill((0, 0, 0))
+
+        self.font.centerText(self.window, 'TB HEXAGON', y=2)
+
+        pygame.display.flip()
+
 
     def controls(self):
-        pass
+        events = pygame.event.get()
+
+        for e in events:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
+                    self.running = False
+
 
     def update(self):
         pass
 
+
+    def start(self):
+        self.running = True
+
+        while self.running:
+            self.render()
+            self.controls()
+            self.update()
+
+
     def quit(self):
         pygame.quit()
-
 
 
 
