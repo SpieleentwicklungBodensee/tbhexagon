30ad2d8abb27aa83262ff4bd74c1e0caa87757a7
drwuro <email@drwuro.com>
2024-01-06 22:26:49 +0100

    added render modes

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import random
 
 from bitmapfont import BitmapFont
 
-
+WIN_W, WIN_H = 1024, 1024
 SCR_W, SCR_H = 256, 256
 
 COLORS = {'red': (255, 0, 0),
@@ -15,12 +15,20 @@ COLORS = {'red': (255, 0, 0),
 DISTANCE = 64
 SPEED = 1.0 / 64
 
+RENDER_MODE = 'led'     # 'plain', 'led'
+
 pygame.display.init()
 
 
 class Game():
     def __init__(self):
-        self.window = pygame.display.set_mode((SCR_W, SCR_H), flags=pygame.SCALED|pygame.FULLSCREEN)
+        if RENDER_MODE == 'plain':
+            self.window = pygame.display.set_mode((SCR_W, SCR_H), flags=pygame.SCALED)
+            self.output = self.window
+        elif RENDER_MODE == 'led':
+            self.window = pygame.display.set_mode((WIN_W, WIN_H))
+            self.output = pygame.Surface((SCR_W, SCR_H))
+
         self.running = False
 
         self.font = BitmapFont('gfx/heimatfont.png', scr_w=SCR_W, scr_h=SCR_H, colors=COLORS.values())
@@ -38,15 +46,7 @@ class Game():
 
 
     def render(self):
-        self.window.fill((0, 0, 0))
-
-        #for i in range(4):
-        #    scale_f = ((4.0 / SCR_W) * ((self.tick + 32 * i) % 128)) ** 2
-        #    #scale_f = 1
-        #    scaled = pygame.transform.scale(self.logo, (SCR_W * scale_f, SCR_H * scale_f))
-        #    scaled.set_alpha(scale_f / 2 * 255)
-        #    scaled = pygame.transform.rotate(scaled, self.tick)
-        #    self.window.blit(scaled, ((SCR_W - scaled.get_width()) / 2, (SCR_H - scaled.get_height()) / 2))
+        self.output.fill((0, 0, 0))
 
         for dist, rot in self.logos:
             size = 1.0 / dist * DISTANCE
@@ -56,7 +56,7 @@ class Game():
             if size <= 1:
                 size = 1
             if size >= SCR_W * 4:
-                self.window.fill((0,0,0))
+                self.output.fill((0,0,0))
                 continue
 
             size = int(size)
@@ -76,20 +76,26 @@ class Game():
             x = (SCR_W - scaled.get_width()) / 2
             y = (SCR_H - scaled.get_height()) / 2
 
-            self.window.blit(scaled, (x, y))
+            self.output.blit(scaled, (x, y))
 
         if int(time.time() * 1000) % 500 < 250:
             title_color = COLORS['red']
         else:
             title_color = COLORS['white']
 
-        self.font_huge.centerText(self.window, 'TOOLBOX', y=2, fgcolor=title_color)
-        self.font_huge.centerText(self.window, 'HEXAGON', y=3, fgcolor=title_color)
+        self.font_huge.centerText(self.output, 'TOOLBOX', y=2, fgcolor=title_color)
+        self.font_huge.centerText(self.output, 'HEXAGON', y=3, fgcolor=title_color)
 
-        self.font.drawText(self.window, 'HI', x=1, y=SCR_H/8-2, fgcolor=COLORS['white'])
-        self.font.drawText(self.window, '00000', x=1, y=SCR_H/8-1, fgcolor=COLORS['white'])
-        self.font.drawText(self.window, '1UP', x=SCR_W/8-4, y=SCR_H/8-2, fgcolor=COLORS['white'])
-        self.font.drawText(self.window, '00000', x=SCR_W/8-6, y=SCR_H/8-1, fgcolor=COLORS['white'])
+        self.font.drawText(self.output, 'HI', x=1, y=SCR_H/8-2, fgcolor=COLORS['white'])
+        self.font.drawText(self.output, '00000', x=1, y=SCR_H/8-1, fgcolor=COLORS['white'])
+        self.font.drawText(self.output, '1UP', x=SCR_W/8-4, y=SCR_H/8-2, fgcolor=COLORS['white'])
+        self.font.drawText(self.output, '00000', x=SCR_W/8-6, y=SCR_H/8-1, fgcolor=COLORS['white'])
+
+        # compose and zoom
+        if RENDER_MODE == 'plain':
+            pass
+        elif RENDER_MODE == 'led':
+            pygame.transform.scale(self.output, (WIN_W, WIN_H), self.window)
 
         pygame.display.flip()
 
